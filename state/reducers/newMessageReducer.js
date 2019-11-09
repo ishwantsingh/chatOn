cecdd5b6f9ca7de5f1d2a85b5eed6c7fc7415ce2
Ishwant Singh <ishwant8@gmail.com>
2019-11-09 23:35:36 +0530

    feat: new message added at front of messages array in reducer

--- state/reducers/newMessageReducer.js
+++ state/reducers/newMessageReducer.js
@@ -5,7 +5,11 @@ import dummyMessages from "../dummyData";
 export default function messageReducer(state = dummyMessages, action) {
   switch (action.type) {
     case "NEW_MESSAGE":
-      return state.concat(action.payload);
+      console.log("action_payload=>", action.payload);
+      state.unshift(action.payload);
+      return state;
+    // return action.payload.concat(state);
+
     // return {
     //   ...state,
     //   text: action.payload.text,
