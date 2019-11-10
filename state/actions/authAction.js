89b6df31df1d1909118a4fccd669ddcce1a70dc8
Ishwant Singh <ishwant8@gmail.com>
2019-11-11 00:41:53 +0530

    feat: made auth actions.

---
+++ state/actions/authAction.js
@@ -0,0 +1,67 @@
+export const login = () => {
+  return dispatch => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      })
+      //   .then(() => {
+      //       dispatch({type: "AUTH_SUCCESS", payload: { token, user}})
+      //   })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        dispatch({ type: "AUTH_SUCCESS", payload: { token, user } });
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        dispatch({ type: "AUTH_FAIL", payload: { errorCode, errorMessage } });
+      });
+  };
+};
+
+export const logout = () => {
+  return () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {
+        // Sign-out successful.
+      })
+      .catch(function(error) {
+        // An error happened.
+        console.log(error);
+      });
+  };
+};
