2f56147a5f88e7e26ec7b075a3f159b5fb9b752d
Ishwant Singh <ishwant8@gmail.com>
2019-11-10 23:58:27 +0530

    feat: added firebase config.

---
+++ fb/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAS4-FyNAsrooEWL5E-B2dLbc6hgq6K5Lk",
+  authDomain: "chaton-a3c2b.firebaseapp.com",
+  databaseURL: "https://chaton-a3c2b.firebaseio.com",
+  projectId: "chaton-a3c2b",
+  storageBucket: "chaton-a3c2b.appspot.com",
+  messagingSenderId: "915834754270",
+  appId: "1:915834754270:web:d667a5242517cdd0ec1e4f",
+  measurementId: "G-QWMJQYKETJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+export default db;
