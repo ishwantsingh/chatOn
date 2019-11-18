f58ebd526237c01b746e954f365bc6e0cf98e79c
Ishwant Singh <ishwant8@gmail.com>
2019-11-18 21:03:10 +0530

    update: cleaned HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,42 +6,17 @@ import { connect } from "react-redux";
 
 import newMessageAction from "../state/actions/newMessageAction";
 
-let user = {
+const user1 = {
   _id: 1,
   name: "Developer"
-};
-
-let user1 = {
-  _id: 1,
-  name: "Developer"
-};
-
-let user2 = {
-  _id: 2,
-  name: "React Native"
 };
 
 class HomeScreen extends React.Component {
-  // state = {
-  //   messages: this.props.messages,
-  //   newMessage: {},
-  //   user: {},
-  //   user1: {
-  //     _id: 1,
-  //     name: "Developer"
-  //   },
-  //   user2: {
-  //     _id: 2,
-  //     name: "React Native"
-  //   }
-  // };
-
   constructor(props) {
     super(props);
     this.state = {
       messages: this.props.messages,
       newMessage: {},
-      user: {},
       user1: {
         _id: 1,
         name: "Developer"
@@ -54,45 +29,16 @@ class HomeScreen extends React.Component {
   }
 
   componentDidUpdate() {
-    // if (this.props.messages.length % 2 == 0) {
-    //   console.log("even length", this.props.messages.length);
-    //   user = user1;
-    // } else if (this.props.messages.length % 2 !== 0) {
-    //   console.log("odd length", this.props.messages.length);
-    //   user = user2;
-    // }
-
     if (this.props.messages.length % 2 == 0) {
-      console.log("evenLength =>", this.props.messages.length);
-      user = user1;
       this.props.newMessageAction(this.state.newMessage, this.state.user1);
       console.log("boo00001");
     } else if (this.props.messages.length % 2 !== 0) {
-      console.log("oddLength =>", this.props.messages.length);
-      user = user2;
       this.props.newMessageAction(this.state.newMessage, this.state.user2);
       console.log("boo00002");
     }
-    // this.setState({ messages: this.props.messages });
-    //  this.props.newMessageAction(this.state.newMessage, this.state.user);
-  }
-
-  componentWillReceiveProps() {
-    console.log("KKKKKKKKKKKK");
-    this.setState({ messages: this.props.messages });
   }
 
   updateScreen = (messages = []) => {
-    //console.log("3");
-    // if (this.state.user === this.state.user1) {
-    //   this.setState({
-    //     user: this.state.user2
-    //   });
-    // } else {
-    //   this.setState({
-    //     user: this.state.user1
-    //   });
-    // }
     if (this.props.messages.length % 2 == 0) {
       console.log("messages.user", messages[0].user);
       messages[0].user = this.state.user1;
@@ -105,30 +51,17 @@ class HomeScreen extends React.Component {
   };
 
   onSend(messages) {
-    //  console.log("messages 1", messages);
-    //  console.log("2");
     this.setState({
       newMessage: messages[0].text
     });
     this.updateScreen(messages);
   }
   render() {
-    // this.setUser();
-    if (this.props.messages == []) {
-      user = user1;
-    }
-    //  console.log("THE USER IS", user);
-    console.log("props", this.props.messages);
-
-    console.log("state", this.state.messages);
     return (
       <View style={styles.container}>
         <GiftedChat
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
-          // user={{
-          //  _id: this.state.senderId
-          // }}
           user={user1}
         />
         {Platform.OS === "android" ? <KeyboardSpacer topSpacing={30} /> : null}
