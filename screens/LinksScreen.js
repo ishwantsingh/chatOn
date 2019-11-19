7afde74338ec4c63de8c2a13b50221084ccde9a7
Ishwant Singh <ishwant8@gmail.com>
2019-11-19 16:08:12 +0530

    addition: action creators and actiontypes updated for login with email and pass.

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -13,6 +13,87 @@ function LinksScreen(props) {
   );
 }
 
+// class Login extends Component {
+//   state = { email: "", password: "" };
+
+//   handleEmailChange = ({ target }) => {
+//     this.setState({ email: target.value });
+//   };
+
+//   handlePasswordChange = ({ target }) => {
+//     this.setState({ password: target.value });
+//   };
+
+//   handleSubmit = () => {
+//     const { dispatch } = this.props;
+//     const { email, password } = this.state;
+
+//     dispatch(loginUser(email, password));
+//   };
+
+//   render() {
+//     const { classes, loginError, isAuthenticated } = this.props;
+//     if (isAuthenticated) {
+//       return <Redirect to="/" />;
+//     } else {
+//       return (
+//         <Container component="main" maxWidth="xs">
+//           <Paper className={classes.paper}>
+//             <Avatar className={classes.avatar}>
+//               <LockOutlinedIcon />
+//             </Avatar>
+//             <Typography component="h1" variant="h5">
+//               Sign in
+//             </Typography>
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               fullWidth
+//               id="email"
+//               label="Email Address"
+//               name="email"
+//               onChange={this.handleEmailChange}
+//             />
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               fullWidth
+//               name="password"
+//               label="Password"
+//               type="password"
+//               id="password"
+//               onChange={this.handlePasswordChange}
+//             />
+//             {loginError && (
+//               <Typography component="p" className={classes.errorText}>
+//                 Incorrect email or password.
+//               </Typography>
+//             )}
+//             <Button
+//               type="button"
+//               fullWidth
+//               variant="contained"
+//               color="primary"
+//               className={classes.submit}
+//               onClick={this.handleSubmit}
+//             >
+//               Sign In
+//             </Button>
+//           </Paper>
+//         </Container>
+//       );
+//     }
+//   }
+// }
+
+// function mapStateToProps(state) {
+//   return {
+//     isLoggingIn: state.auth.isLoggingIn,
+//     loginError: state.auth.loginError,
+//     isAuthenticated: state.auth.isAuthenticated
+//   };
+// }
+
 LinksScreen.navigationOptions = {
   title: "Links"
 };
--- state/actions/authAction.js
+++ state/actions/authAction.js
@@ -1,117 +1,109 @@
 import { myFirebase } from "../../fb/config";
-import firebase from "firebase";
+// import firebase from "firebase";
 
-// export const login = () => {
-//   return dispatch => {
-//     var provider = new firebase.auth.GoogleAuthProvider();
-//     firebase
-//       .auth()
-//       .signInWithPopup(provider)
-//       .then(function(result) {
-//         // This gives you a Google Access Token. You can use it to access the Google API.
-//         var token = result.credential.accessToken;
-//         // The signed-in user info.
-//         var user = result.user;
-//         // ...
-//       })
-//       //   .then(() => {
-//       //       dispatch({type: "AUTH_SUCCESS", payload: { token, user}})
-//       //   })
-//       .catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // The email of the user's account used.
-//         var email = error.email;
-//         // The firebase.auth.AuthCredential type that was used.
-//         var credential = error.credential;
-//         // ...
-//       });
-//     firebase
-//       .auth()
-//       .getRedirectResult()
-//       .then(function(result) {
-//         if (result.credential) {
-//           // This gives you a Google Access Token. You can use it to access the Google API.
-//           var token = result.credential.accessToken;
-//           // ...
-//         }
-//         // The signed-in user info.
-//         var user = result.user;
-//         dispatch({ type: "AUTH_SUCCESS", payload: { user, token } });
-//       })
-//       .catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // The email of the user's account used.
-//         var email = error.email;
-//         // The firebase.auth.AuthCredential type that was used.
-//         var credential = error.credential;
-//         // ...
-//         dispatch({
-//           type: "AUTH_FAIL",
-//           payload: { err: `${errorCode}: ${errorMessage}` }
-//         });
-//       });
-//   };
-// };
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
-// export const logout = () => {
-//   return () => {
-//     firebase
-//       .auth()
-//       .signOut()
-//       .then(function() {
-//         // Sign-out successful.
-//       })
-//       .catch(function(error) {
-//         // An error happened.
-//         console.log(error);
-//       });
-//   };
-// };
+export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAILURE = "LOGOUT_FAILURE";
 
-export const login = () => dispatch => {
-  var provider = new firebase.auth.GoogleAuthProvider();
+export const VERIFY_REQUEST = "VERIFY_REQUEST";
+export const VERIFY_SUCCESS = "VERIFY_SUCCESS";
+
+const requestLogin = () => {
+  return {
+    type: LOGIN_REQUEST
+  };
+};
+
+const receiveLogin = user => {
+  return {
+    type: LOGIN_SUCCESS,
+    user
+  };
+};
+
+const loginError = () => {
+  return {
+    type: LOGIN_FAILURE
+  };
+};
+
+const requestLogout = () => {
+  return {
+    type: LOGOUT_REQUEST
+  };
+};
+
+const receiveLogout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  };
+};
+
+const logoutError = () => {
+  return {
+    type: LOGOUT_FAILURE
+  };
+};
+
+const verifyRequest = () => {
+  return {
+    type: VERIFY_REQUEST
+  };
+};
+
+const verifySuccess = () => {
+  return {
+    type: VERIFY_SUCCESS
+  };
+};
+
+export const login = (email, password) => dispatch => {
+  dispatch(requestLogin());
   myFirebase
     .auth()
-    .signInWithPopup(provider)
-    .then(function(result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-      console.log("user", user);
+    .signInWithEmailAndPassword(email, password)
+    .then(user => {
       dispatch({ type: "AUTH_SUCCESS", payload: { token, user } });
+      dispatch(receiveLogin(user));
     })
-    //   .then(() => {
-    //       dispatch({type: "AUTH_SUCCESS", payload: { token, user}})
-    //   })
-    .catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
+    .catch(error => {
+      //Do something with the error if you want!
+      console.log("error", error);
+      dispatch(loginError());
     });
-  myFirebase
-    .auth()
-    .getRedirectResult()
-    .then(function(result) {
-      if (result.credential) {
-        var token = result.credential.accessToken;
-      }
-      var user = result.user;
-      dispatch({ type: "AUTH_SUCCESS", payload: { user, token } });
-    })
-    .catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-      dispatch({
-        type: "AUTH_FAIL",
-        payload: { err: `${errorCode}: ${errorMessage}` }
-      });
-    });
+  //   .then(() => {
+  //       dispatch({type: "AUTH_SUCCESS", payload: { token, user}})
+  //   })
+  // .catch(function(error) {
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   var email = error.email;
+  //   var credential = error.credential;
+  // });
+  // myFirebase
+  //   .auth()
+  //   .getRedirectResult()
+  //   .then(function(result) {
+  //     if (result.credential) {
+  //       var token = result.credential.accessToken;
+  //     }
+  //     var user = result.user;
+  //     dispatch({ type: "AUTH_SUCCESS", payload: { user, token } });
+  //   })
+  //   .catch(function(error) {
+  //     var errorCode = error.code;
+  //     var errorMessage = error.message;
+  //     var email = error.email;
+  //     var credential = error.credential;
+  //     dispatch({
+  //       type: "AUTH_FAIL",
+  //       payload: { err: `${errorCode}: ${errorMessage}` }
+  //     });
+  //   });
 };
 
 export const logout = () => dispatch => {
@@ -120,18 +112,23 @@ export const logout = () => dispatch => {
     .signOut()
     .then(function() {
       console.log("signout success");
+      dispatch(receiveLogout());
     })
     .catch(function(error) {
       // An error happened.
-      console.log(error);
+      console.log("signout error", error);
+      dispatch(logoutError());
     });
 };
 
 export const verifyAuth = () => dispatch => {
+  dispatch(verifyRequest());
   myFirebase.auth().onAuthStateChanged(user => {
     if (user !== null) {
       console.log("auth change user", user);
+      dispatch(receiveLogin(user));
     }
     console.log("okie dokie");
+    dispatch(verifySuccess());
   });
 };
