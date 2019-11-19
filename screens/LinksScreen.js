3c32adbe20f7401541d5dbe46c84dee874c98549
Ishwant Singh <ishwant8@gmail.com>
2019-11-19 17:46:41 +0530

    update: cleaned comments in Linkscreen.js

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -11,64 +11,32 @@ import {
 import { login } from "../state/actions/authAction";
 import { connect } from "react-redux";
 
-// function LinksScreen(props) {
-//   return (
-//     <ScrollView style={styles.container}>
-//       <Text>Log In Please</Text>
-//       <Button onPress={props.login} title="Login" />
-//     </ScrollView>
-//   );
-// }
-
 function LinksScreen(props) {
   const [email, handleEmailChange] = React.useState("");
   const [password, handlePasswordChange] = React.useState("");
 
-  // state = { email: "", password: "" };
-
-  // handleEmailChange = text => {
-  //   console.log("text is=>", text);
-  //   this.setState({ email: text });
-  // };
-
-  // handlePasswordChange = text => {
-  //   this.setState({ password: text });
-  // };
-
   handleSubmit = e => {
     e.preventDefault();
-    // const { dispatch } = this.props;
-    //  const { email, password } = this.state;
     console.log("email is=>", email);
     console.log("pass is=>", password);
     props.login(email, password);
   };
 
-  // render() {
   const { loginError, isAuthenticated } = props;
-  // if (isAuthenticated) {
-  //   return <Redirect to="/" />;
-  // } else {
+
   return (
-    // <Container component="main" maxWidth="xs">
     <View>
       <View>
-        {/* <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar> */}
         <Text>Sign in</Text>
         <TextInput
           textContentType="emailAddress"
           placeholder="Email Address"
-          //   name="email"
           value={email}
           onChangeText={text => handleEmailChange(text)}
         />
         <TextInput
-          //   name="password"
           placeholder="Password"
           textContentType="password"
-          // id="password"
           value={password}
           onChangeText={text => handlePasswordChange(text)}
         />
@@ -79,9 +47,7 @@ function LinksScreen(props) {
       </View>
     </View>
   );
-  // }
 }
-// }
 
 LinksScreen.navigationOptions = {
   title: "Links"
@@ -96,7 +62,6 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = dispatch => {
-  //123
   return {
     login: (email, password) => dispatch(login(email, password))
   };
