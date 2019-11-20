a3a53986b5ed8f41abc3cc964f813675e9dd4b68
Ishwant Singh <ishwant8@gmail.com>
2019-11-20 15:44:57 +0530

    update: setData reducer made.

--- state/reducers/setDataReducer.js
+++ state/reducers/setDataReducer.js
@@ -1,15 +1,35 @@
-// const initialState = {
-//   superFinal: []
-// };
+import {
+  GET_DATA_SUCCESS,
+  GET_DATA_ERROR,
+  GETTING_DATA
+} from "../actions/setDataAction";
 
-// export default function setDataReducer(state = initialState, action) {
-//   switch (action.type) {
-//     case "SET_DATA":
-//       return {
-//         ...state,
-//         superFinal: action.payload.superFinal
-//       };
-//     default:
-//       return state;
-//   }
-// }
+const initialState = {
+  data: [],
+  dataErr: null,
+  gettingData: false
+};
+
+export default function setDataReducer(state = initialState, action) {
+  switch (action.type) {
+    case GETTING_DATA:
+      return {
+        ...state,
+        gettingData: true
+      };
+    case GET_DATA_SUCCESS:
+      return {
+        ...state,
+        gettingData: false,
+        data: action.payload
+      };
+    case GET_DATA_ERROR:
+      return {
+        ...state,
+        gettingData: false,
+        dataErr: action.payload
+      };
+    default:
+      return state;
+  }
+}
