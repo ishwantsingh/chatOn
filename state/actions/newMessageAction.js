8fbc18e4cc0e32f058da6fbccdbfb546b5fb1423
Ishwant Singh <ishwant8@gmail.com>
2019-11-20 23:01:10 +0530

    update: sendMessage action updated. not working.

--- state/actions/newMessageAction.js
+++ state/actions/newMessageAction.js
@@ -1,19 +1,58 @@
 import uuid from "uuid";
+import { db } from "../../fb/config";
 
-// const getInfo = (cal, carb, fat, fiber, mineral, protein) => {
-//   return {
-//     type: "GET_INFO",
-//     payload: {
-//       cal,
-//       carb,
-//       fat,
-//       fiber,
-//       mineral,
-//       protein
-//     }
-//   };
-// };
-// export default getInfo;
+export const NEW_MES_SENT = "NEW_MES_SENT";
+export const NEW_MES_ERR = "NEW_MES_ERR";
+export const SENDING_NEW_MES = "SENDING_NEW_MES";
+
+const sentMessage = docRef => {
+  return {
+    type: NEW_MES_SENT,
+    payload: { docId: docRef }
+  };
+};
+
+const sendError = err => {
+  return {
+    type: NEW_MES_ERR,
+    payload: { err }
+  };
+};
+const sendingMessage = () => {
+  return {
+    type: SENDING_NEW_MES
+  };
+};
+
+export function sendMessgae(text, userDetails, senderId, recieverId) {
+  console.log("check?");
+  return dispatch => {
+    console.log("sooka blooka");
+    dispatch(sendingMessage());
+    console.log("baangu baangu");
+    var doc_id = `${senderId}+${recieverId}`;
+
+    db.collection("chatRooms")
+      .doc(doc_id)
+      .collection("messages")
+      .doc()
+      .set({
+        text: text,
+        createdAt: new Date(),
+        user: userDetails,
+        senderId: senderId,
+        recieverId: recieverId
+      })
+      .then(docRef => {
+        console.log("Document written with ID: ", docRef.id);
+        dispatch(sentMessage(docRef.id));
+      })
+      .catch(err => {
+        dispatch(sendError(err));
+        console.log("send message err=>", err);
+      });
+  };
+}
 
 export default function newMessageAction(text, user) {
   return {
